9a7fa0732c67561075e3211b054cd2cf2ad2f3fc
HieuShinDepTrai <hieubglgmtcl02@gmail.com>
2022-01-05 21:18:42 +0700

    h203 fix

--- J1.S.H202/src/j1/s/h202/Main.java
+++ J1.S.H202/src/j1/s/h202/Main.java
@@ -1,29 +1,47 @@
 package j1.s.h202;
 
+import java.util.Scanner;
+
 public class Main {
-//    String ValidateInput(String Mess, String reg) {
-//        Scanner SC = new Scanner(System.in);
-//        String ret = "";
-//        do {
-//            System.out.println("Mess");
-//            ret = SC.nextLine();
-//            if(ret.matches(reg)) {
-//                break;
-//            }
-//            System.out.println("Something went wrong, please try again!");
-//        } while (true);
-//        return reg;
-//    }
-    
+
+    private static Scanner sc;
+
+    public static void clearBuffer() {
+        sc = new Scanner(System.in);
+    }
+
+    public static String getString(String msg, boolean isLoop) {
+        String result = null;
+
+        do {
+            System.out.print(msg);
+            clearBuffer();
+
+            try {
+                result = sc.nextLine();
+                if (result.trim().isEmpty()) {
+                    System.err.println("You must enter a not empty string");
+                } else {
+                    break;
+                }
+            } catch (Exception e) {
+                System.err.println("You must enter a string");
+            }
+        } while (isLoop);
+
+        return result;
+    }
+
     public static String printReverse(String S) {
         StringBuilder str = new StringBuilder(S);
 //        System.out.println(str.reverse());
         return str.reverse().toString();
     }
-    
+
     public static void main(String[] args) {
-        String s = "Hello There!";
+        String s = "";
+        s = getString("Enter a String: ", s.isEmpty() == true);
         System.out.println(printReverse(s));
     }
- 
+
 }
