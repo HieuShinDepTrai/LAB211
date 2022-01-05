676fbdca7550ab8954e39f1b80a9ab3cb5d64c30
HieuShinDepTrai <hieubglgmtcl02@gmail.com>
2022-01-05 21:45:32 +0700

    h203 fix

--- J1.S.H203/src/j1/s/h203/Main.java
+++ J1.S.H203/src/j1/s/h203/Main.java
@@ -35,6 +35,21 @@ public class Main {
     public static void main(String args[]) {
         String input = "";
         input = getString("Enter a String: ", input.isEmpty() == true);
+        input = input.toLowerCase();
+        // Split string by sentence (. and space) space co hoac khong cung duoc
+        String[] src = input.split("\\. *");
+        String[] temp = new String[src.length];
+        for (int i = 0; i < src.length; i++) {
+            if (src[i].isEmpty() == false) {
+                src[i] = Character.toUpperCase(src[i].charAt(0)) + src[i].substring(1);
+            }
+            temp[src.length - 1 - i] = src[i];
+        }
+        input = "";
+        for (int i = 0; i < src.length - 1; i++) {
+            input += (temp[i] + ". ");
+        }
+        input += temp[src.length - 1];
+        System.out.println(input); 
     }
-
 }
