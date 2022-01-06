c502e9c3df882399772e10db3959b1aadb90fc71
HieuShinDepTrai <hieubglgmtcl02@gmail.com>
2022-01-06 11:03:09 +0700

    P0001

--- J1.S.P0020/src/j1/s/p0020/Main.java
+++ J1.S.P0020/src/j1/s/p0020/Main.java
@@ -1,84 +1,44 @@
 package j1.s.p0020;
 
-import java.util.Scanner;
-
 public class Main {
 
-    private static int inputInt() {
-        //loop until user input correct
-        Scanner sc = new Scanner(System.in);
-        int n = 0;
-        boolean check = true;
-        //Nhap kich thuowc mang
-        while (check) {
-            try {
-                n = Integer.parseInt(sc.nextLine().trim());
-                check = false;
-            } catch (NumberFormatException e) {
-                System.err.println("Please input a number: ");
-            }
-        }
-        return n;
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = 0;
-        boolean check = true;
-        //Nhap kich thuowc mang
-        System.out.println("Input size of array(positive decimal number): ");
-        while (check) {
-            try {
-                n = Integer.parseInt(sc.nextLine().trim());
-                if (n > 0) {
-                    check = false;
-                } else {
-                    throw new Exception("Please input a positive decimal number: ");
-                }
-            } catch (NumberFormatException e) {
-                System.err.println("Please input a positive decimal number: ");
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
+        ValidateInput IntegerNumber = new ValidateInput();
 
         //khoi tao va Nhap mang
+        int n = IntegerNumber.CheckInt("Enter number of array: ", Integer.MIN_VALUE, Integer.MAX_VALUE);
         int[] a = new int[n];
         for (int i = 0; i < n; i++) {
-            System.out.print("Enter a[" + i + "]: ");
-            a[i] = inputInt();
+            a[i] = IntegerNumber.CheckInt("Enter a[" + i + "]: ", Integer.MIN_VALUE, Integer.MAX_VALUE);
         }
 
         //in ra mang chua sort
-        System.out.print("\nUnsorted array: ");
-        int i;
-        for (i = 0; i < a.length - 1; i++) {
+        System.out.print("Unsorted array: [");
+        
+        for (int i = 0; i < a.length - 1; i++) {
             System.out.print(a[i] + ", ");
         }
-        System.out.print(a[i]);
+        System.out.println(a[a.length - 1] + "]");
 
-        //ham Selection sort
-        for (i = 0; i < n - 1; i++) {
-            // tim so be nhat trong mang
-            int min_idx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (a[j] < a[min_idx]) {
-                    min_idx = j;
-                }
-            }
-
-            // Swap vs phan tu dau tien
-            int temp = a[min_idx];
-            a[min_idx] = a[i];
-            a[i] = temp;
-        }
-        System.out.println();
-        System.out.print("Sorted array: ");
-        for (i = 0; i < a.length - 1; i++) {
+        //ham selection sort
+        int temp;
+        for(int i = 0; i < a.length; i++)  
+       {  
+           for(int j = i + 1; j < a.length; j++)  
+           {  
+               if(a[i] > a[j])  
+               {  
+                   temp = a[i];  
+                   a[i] = a[j];  
+                   a[j] = temp;  
+               }  
+           }  
+       }  
+       
+        System.out.print("Sorted array: [");
+        for (int i = 0; i < a.length - 1; i++) {
             System.out.print(a[i] + ", ");
         }
-        System.out.println(a[i]);
-
+        System.out.println(a[a.length - 1] + "]");
     }
-
 }
