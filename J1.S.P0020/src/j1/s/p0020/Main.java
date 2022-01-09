134738bf56aed8b63121cecb44d315239b542bc5
HieuShinDepTrai <hieunvhe163104@fpt.edu.vn>
2022-01-09 14:32:51 +0700

    fix sort

--- J1.S.P0020/src/j1/s/p0020/Main.java
+++ J1.S.P0020/src/j1/s/p0020/Main.java
@@ -1,44 +1,37 @@
 package j1.s.p0020;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         ValidateInput IntegerNumber = new ValidateInput();
 
-        //khoi tao va Nhap mang
         int n = IntegerNumber.CheckInt("Enter number of array: ", Integer.MIN_VALUE, Integer.MAX_VALUE);
         int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = IntegerNumber.CheckInt("Enter a[" + i + "]: ", Integer.MIN_VALUE, Integer.MAX_VALUE);
-        }
-
-        //in ra mang chua sort
-        System.out.print("Unsorted array: [");
         
-        for (int i = 0; i < a.length - 1; i++) {
-            System.out.print(a[i] + ", ");
+        Random rd = new Random();
+        for (int i = 0; i < n; i++) {
+            a[i] = rd.nextInt(n + 1);
         }
-        System.out.println(a[a.length - 1] + "]");
 
-        //ham selection sort
+        System.out.print("Unsorted array: ");
+
+        System.out.println(Arrays.toString(a));
+
         int temp;
-        for(int i = 0; i < a.length; i++)  
-       {  
-           for(int j = i + 1; j < a.length; j++)  
-           {  
-               if(a[i] > a[j])  
-               {  
-                   temp = a[i];  
-                   a[i] = a[j];  
-                   a[j] = temp;  
-               }  
-           }  
-       }  
-       
-        System.out.print("Sorted array: [");
-        for (int i = 0; i < a.length - 1; i++) {
-            System.out.print(a[i] + ", ");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
         }
-        System.out.println(a[a.length - 1] + "]");
+
+        System.out.print("Sorted array: ");
+        System.out.println(Arrays.toString(a));
     }
 }
--- j1.s.p0001/src/j1/s/p0001/BubbleSort.java
+++ j1.s.p0001/src/j1/s/p0001/BubbleSort.java
@@ -1,29 +1,22 @@
 package j1.s.p0001;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class BubbleSort {
 
     public static void main(String[] args) {
         ValidateInput IntegerNumber = new ValidateInput();
 
-        //khoi tao va Nhap mang
         int n = IntegerNumber.CheckInt("Enter number of array: ", Integer.MIN_VALUE, Integer.MAX_VALUE);
         int[] a = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = IntegerNumber.CheckInt("Enter a[" + i + "]: ", Integer.MIN_VALUE, Integer.MAX_VALUE);
         }
 
-        //in ra mang chua sort
-        System.out.print("Unsorted array: [");
-        int i;
-        for (i = 0; i < a.length - 1; i++) {
-            System.out.print(a[i] + ", ");
-        }
-        System.out.println(a[i] + "]");
+        System.out.print("Unsorted array: ");
+        System.out.println(Arrays.toString(a));
 
-        //ham bubble sort
-        for (i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length - i - 1; j++) {
                 if (a[j] > a[j + 1]) {
                     int temp = a[j];
@@ -33,11 +26,8 @@ public class BubbleSort {
             }
         }
        
-        System.out.print("Sorted array: [");
-        for (i = 0; i < a.length - 1; i++) {
-            System.out.print(a[i] + ", ");
-        }
-        System.out.println(a[i] + "]");
+        System.out.print("Sorted array: ");
+        System.out.println(Arrays.toString(a));
     }
 
 }
